08c6a72d49a76bb978d73078d8b9aa2c43f4fb85
Joyce Yu <joyceyu96@gmail.com>
2023-01-25 13:20:39 -0800

    completed trees easy

--- Python/Trees/Easy/maximum_depth_of_binary_tree.py
+++ Python/Trees/Easy/maximum_depth_of_binary_tree.py
@@ -15,3 +15,13 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        def dfs(root):
+            if not root:
+                return 0
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            return 1+max(left,right)
+
+        return dfs(root)
